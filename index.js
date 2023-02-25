8a06f3f4ae999c6fab7a9e781af684365e1453dd
Faisal Alam <faisal.alam.1999@gmail.com>
2023-02-25 19:20:07 +0530

    refactored routes and added config for env variables

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require("express");
-const https=require('https');
-const fs=require('fs');
+const https = require('https');
+const config=require('config')
 
 const app = express();
 
-app.get("/", (req,res)=>{
-    res.send("welcome to irrigation-system")
-})
-// const PORT = process.env.PORT || 5000;
-const httpsPORT=process.env.httpsPORT || 3443;
 
-const sslServer=https.createServer({
-    key: fs.readFileSync("cert/key.pem"),
-    cert: fs.readFileSync("cert/cert.pem")
+require("./startup/routes")(app);
+
+
+
+
+// const PORT = process.env.PORT || 5000;
+const httpsPORT = process.env.httpsPORT || 3443;
+
+const sslServer = https.createServer({
+    key: Buffer.from(config.get("sslKey"), 'base64').toString('ascii'),
+    cert: Buffer.from(config.get("sslCert"), 'base64').toString('ascii')
 }, app)
 
-sslServer.listen(httpsPORT, ()=> console.log(`SSL Server active on port ${httpsPORT}`))     //https server
+sslServer.listen(httpsPORT, () => console.log(`SSL Server active on port ${httpsPORT}`))     //https server
 // app.listen(PORT, () => console.log(`Server Active on port ${PORT}`));    //for http
