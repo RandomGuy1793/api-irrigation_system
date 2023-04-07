23bae834c4f36be3b70579007a292bea2ada6bcc
Faisal Alam <faisal.alam.1999@gmail.com>
2023-04-07 23:13:17 +0530

    fixed auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,8 +5,8 @@ const tokenOffset = 7;
 
 module.exports = function (req, res, next) {
   const received_token = req.header("authorization");
+  if (!received_token) return res.status(401).send("Access denied, no token provided.");
   const token = received_token.substr(tokenOffset, received_token.length);
-  if (!token) return res.status(401).send("Access denied, no token provided.");
   try {
     const jwtKey = config.get("jwtKey");
     const verified = jwt.verify(token, jwtKey);
